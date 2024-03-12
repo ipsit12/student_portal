148c7b9b0697e8d673d4c2e96f06e96b89a99adc
ipsit12 <128500529+ipsit12@users.noreply.github.com>
2024-03-12 23:31:50 +0530

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,191 @@
+<!-- partial:./partials/_sidebar.html -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+  <ul class="nav">
+    <li class="nav-item sidebar-category">
+      <p>Admin</p>
+      <span></span>
+    </li>
+
+    <!-- <li class="nav-item sidebar-category">
+          <p>Components</p>
+          <span></span>
+        </li> -->
+    <li class="nav-item">
+      <!-- <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+            <i class="mdi mdi-palette menu-icon"></i>
+            <span class="menu-title">UI Elements</span>
+            <i class="menu-arrow"></i>
+          </a> -->
+      <!-- <div class="collapse" id="ui-basic">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
+            </ul>
+          </div> -->
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" href="index1.php">
+        <i class="fa-solid fa-database"></i>
+        <span class="menu-title mx-2">Dashboard</span>
+      </a>
+    </li>
+
+    <!-- <li class="nav-item">
+          <a class="nav-link" href="dueofstudent.php">
+            <i class="mdi mdi-grid-large menu-icon"></i>
+            <span class="menu-title">Add Student Due</span>
+          </a>
+        </li> -->
+
+    <li class="nav-item">
+      <a class="nav-link" href="Employeelist.php">
+        <i class="fa-solid fa-user"></i>
+        <span class="menu-title mx-2">Employee</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="course.php">
+        <i class="fa-solid fa-book"></i>
+        <span class="menu-title mx-2">Course</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="studentenquire.php">
+        <i class="fa-solid fa-user"></i>
+        <span class="menu-title mx-1">Student Enquiry</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="visitorenquire.php">
+        <i class="fa-solid fa-user"></i>
+        <span class="menu-title mx-1">Visitor Enquiry</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="admission.php">
+        <i class="fa-solid fa-graduation-cap"></i>
+        <span class="menu-title mx-2">Admission</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Readmission.php">
+        <i class="fa-solid fa-graduation-cap"></i>
+        <span class="menu-title mx-2">Re-Admission</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="student.php">
+        <i class="fa-solid fa-school"></i>
+        <span class="menu-title mx-2">Student</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="examresult.php">
+        <i class="fa-solid fa-credit-card"></i>
+        <span class="menu-title mx-2">Examresult</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="notice.php">
+        <i class="fa-solid fa-circle-exclamation"></i>
+        <span class="menu-title mx-2">Notice</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="payment.php">
+        <i class="fa-solid fa-credit-card"></i>
+        <span class="menu-title mx-2">Payment</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Not_Paying.php">
+        <i class="fa-solid fa-credit-card"></i>
+        <span class="menu-title mx-2">Irregular Payment</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="counselling.php">
+        <i class="fa-solid fa-globe"></i>
+        <span class="menu-title mx-2">Counselling</span>
+      </a>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="fa-solid fa-gears"></i>
+        <span class="menu-title mx-2">Setting</span>
+      </a>
+      <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        <li><a class="dropdown-item" href="changepassword.php">Change Password</a></li>
+        <li><a class="dropdown-item" href="prefix.php">Prefix</a></li>
+        <li><a class="dropdown-item" href="#">Email Setup</a></li>
+        <li><a class="dropdown-item" href="#">SMS Setup</a></li>
+      </ul>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="fa-solid fa-bug"></i>
+        <span class="menu-title mx-2">Reports</span>
+      </a>
+      <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        <li><a class="dropdown-item" href="course_list.php">Course Details</a></li>
+        <li><a class="dropdown-item" href="student_enquiry_list.php">Student Enquiry Details</a></li>
+        <li><a class="dropdown-item" href="student_details.php">Student Details</a></li>
+        <li><a class="dropdown-item" href="payment_details.php">Payment Details</a></li>
+        <li><a class="dropdown-item" href="counselling_details.php">Counselling</a></li>
+        <li><a class="dropdown-item" href="bankdetails.php">Bank Details</a></li>
+      </ul>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="logout.php">
+        <i class="fas fa-user-circle"></i>
+        <span class="menu-title mx-2">Logout</span>
+      </a>
+    </li>
+    <!-- <li class="nav-item">
+          <a class="nav-link" href="pages/icons/mdi.html">
+            <i class="mdi mdi-emoticon menu-icon"></i>
+            <span class="menu-title">Upcoming Events</span>
+          </a>
+        </li> -->
+    <!-- <li class="nav-item sidebar-category">
+          <p>Pages</p>
+          <span></span>
+        </li> -->
+    <!-- <li class="nav-item">
+          <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+            <i class="mdi mdi-account menu-icon"></i>
+            <span class="menu-title">Exam Result</span> -->
+    <!-- <i class="menu-arrow"></i> -->
+    </a>
+    <!-- <div class="collapse" id="auth">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> Login 2 </a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/register-2.html"> Register 2 </a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/lock-screen.html"> Lockscreen </a></li>
+            </ul>
+          </div> -->
+    </li>
+    <!-- <li class="nav-item sidebar-category">
+          <p>Apps</p>
+          <span></span>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="docs/documentation.html">
+            <i class="mdi mdi-file-document-box-outline menu-icon"></i>
+            <span class="menu-title">Documentation</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="http://www.bootstrapdash.com/demo/spica/template/">
+            <button class="btn bg-danger btn-sm menu-title">Upgrade to pro</button>
+          </a>
+        </li> -->
+  </ul>
+</nav>
+<!-- partial -->
